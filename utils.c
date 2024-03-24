41dbc149a688087a5cb8cc72c4a2f984bd2f3a12
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-03-24 02:42:01 +0000

    Update utils.c

--- utils.c
+++ utils.c
@@ -44,9 +44,93 @@ void iter_loop(t_fractol *fractol)
 
 void set_color(t_fractol *fractol, int color)
 {
+    // fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    // fractol->ch[fractol->index] = (fractol->iter * fractol->color - 473 + 765) * color;
+    // fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color - 449 + 765) * color;
+    // fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color - 473 + 765) * color;
+    // fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color - 416 + 765) * color;
+    // fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color - 489 + 765) * color;
+    // fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color - 475 + 765) * color;
+    // fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color - 435 + 765) * color;
+    // fractol->ch[fractol->index + 7] = 0;
+
+    // fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    // fractol->ch[fractol->index] = (fractol->iter * fractol->color - 400 + 765) * color;     // Red
+    // fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color - 300 + 765) * color; // Orange
+    // fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color - 200 + 765) * color; // Yellow
+    // fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color - 100 + 765) * color; // Green
+    // fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color + 0 + 765) * color;   // Blue
+    // fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color + 100 + 765) * color; // Indigo
+    // fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color + 200 + 765) * color; // Violet
+    // fractol->ch[fractol->index + 7] = 0;
+
+    // fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    // fractol->ch[fractol->index] = (fractol->iter * fractol->color + 100 + 765) * color;     // Red
+    // fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color - 50 + 765) * color;  // Green
+    // fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 200 + 765) * color; // Blue
+    // fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color - 150 + 765) * color; // Yellow
+    // fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color + 50 + 765) * color;  // Purple
+    // fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color - 100 + 765) * color; // Cyan
+    // fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color + 150 + 765) * color; // Pink
+    // fractol->ch[fractol->index + 7] = 0;
+
+    // fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    // fractol->ch[fractol->index] = (fractol->iter * fractol->color + 200 + 765) * color;     // Bright Red
+    // fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color + 150 + 765) * color; // Bright Green
+    // fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 100 + 765) * color; // Bright Blue
+    // fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color + 250 + 765) * color; // Bright Yellow
+    // fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color + 50 + 765) * color;  // Bright Purple
+    // fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color + 200 + 765) * color; // Bright Cyan
+    // fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color + 150 + 765) * color; // Bright Pink
+    // fractol->ch[fractol->index + 7] = 0;
+
+    // fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    // fractol->ch[fractol->index] = (fractol->iter * fractol->color + 100 + 765) * color;     // Sky Blue
+    // fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color + 200 + 765) * color; // Sunflower Yellow
+    // fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 150 + 765) * color; // Soft Green
+    // fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color + 250 + 765) * color; // Pink
+    // fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color + 50 + 765) * color;  // Lavender
+    // fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color + 200 + 765) * color; // Pastel Orange
+    // fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color + 100 + 765) * color; // Light Coral
+    // fractol->ch[fractol->index + 7] = 255;
+
+    // fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    // fractol->ch[fractol->index] = (fractol->iter * fractol->color + 100) * color;     // Sky Blue
+    // fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color + 200) * color; // Sunflower Yellow
+    // fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 150) * color; // Soft Green
+    // fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color + 250) * color; // Pink
+    // fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color + 50) * color;  // Lavender
+    // fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color + 200) * color; // Pastel Orange
+    // fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color + 100) * color; // Light Coral
+    // fractol->ch[fractol->index + 7] = 0; // Transparent
+    
+    //dark ghost
     fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
-    fractol->ch[fractol->index] = (fractol->iter * fractol->color + 680 + 179) * color;
-    fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color + 42 + 84) * color;
-    fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 680 + 179) * color;
-    fractol->ch[fractol->index + 3] = 0;
+    fractol->ch[fractol->index] = (fractol->iter * fractol->color + 200) * color;   // Red component (light grey)
+    fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color + 200) * color; // Green component (light grey)
+    fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 200) * color; // Blue component (light grey)
+    fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color + 150) * color; // Red component (light blue)
+    fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color + 220) * color; // Green component (light blue)
+    fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color + 255) * color; // Blue component (light blue)
+    fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color + 255) * color; // Red component (white)
+    fractol->ch[fractol->index + 7] = (fractol->iter * fractol->color + 255) * color; // Green component (white)
+    
+    // //my world
+    // fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    // fractol->ch[fractol->index] = (fractol->iter * fractol->color + 14) * color;   // Red component (light grey)
+    // fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color + 28) * color; // Green component (light grey)
+    // fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 42) * color; // Blue component (light grey with blueish tone)
+    // fractol->ch[fractol->index + 3] = (fractol->iter * fractol->color + 56) * color; // Red component (light blue)
+    // fractol->ch[fractol->index + 4] = (fractol->iter * fractol->color + 70) * color; // Green component (light blue)
+    // fractol->ch[fractol->index + 5] = (fractol->iter * fractol->color + 84) * color; // Blue component (light blue)
+    // fractol->ch[fractol->index + 6] = (fractol->iter * fractol->color + 98) * color; // Red component (white)
+    // fractol->ch[fractol->index + 7] = 0;
+
+    /*fractol->index = (4 * 1000 * fractol->y_y) + fractol->x_x * 4;
+    fractol->ch[fractol->index] = (fractol->iter * fractol->color + 255) * color;   // Red component
+    fractol->ch[fractol->index + 1] = (fractol->iter * fractol->color + 192) * color; // Green component
+    fractol->ch[fractol->index + 2] = (fractol->iter * fractol->color + 203) * color; // Blue component
+    fractol->ch[fractol->index + 7] = 0; // Transparent*/
+
+
 }
