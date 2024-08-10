76d4370a969b1ebf166e0590c421e8f4d7f9bdf4
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-08-10 16:32:42 +0100

    Update handle_input_error.c

--- utils/handle_input_error.c
+++ utils/handle_input_error.c
@@ -1,8 +1,10 @@
 #include "../fractol.h"
 
+// Function to display an error msg & exit
 void	handle_input_error(t_data *data)
 {
 	perror("erreur: not a valid numerical values !");
+	// Free allocated data and exit the program
 	free(data);
 	exit(1);
 }
